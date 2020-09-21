3deb03847678536271662e3d683d25cbc88986f9
jkulovitz <jennifer.kulovitz@gmail.com>
2020-09-21 16:22:56 -0400

    get data from source and show error if exists

--- src/app/equipmentList/equipmentList.component.ts
+++ src/app/equipmentList/equipmentList.component.ts
@@ -1,11 +1,32 @@
-import {Component} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'app-equipment',
     templateUrl: './equipmentList.component.html',
     styleUrls: ['./equipmentList.component.css']
 })
-export class EquipmentListComponent {
-    constructor() {
+export class EquipmentListComponent implements OnInit {
+    equipment = [];
+    errors: boolean = false;
+
+    constructor(private httpClient: HttpClient) {
+    }
+
+    ngOnInit() {
+        this.httpClient.get(`${environment.apiUrl}/equipment`)
+            .subscribe(
+                (data: any[]) => {
+                    let activeEquipment = data.filter(product => product.active);
+                    this.equipment = activeEquipment;
+                    console.log(this.equipment);
+                },
+                error => {
+                    console.log(error);
+                    this.errors = true;
+                }
+                
+            );
     }
 }
